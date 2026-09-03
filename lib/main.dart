f8406b510fcb40368cfbe9218e6b2dd45701465b
vict@r.. <www44victor@gmail.com>
2026-09-03 11:32:13 +0300

    feat: persistent login session, auto-logout after 90 days inactivity

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,27 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'app.dart';
 import 'core/services/update_checker.dart';
 import 'firebase_options.dart';
+import 'state/auth_state.dart';
 import 'state/client_preferences_state.dart';
+
+class _AppLifecycleObserver extends WidgetsBindingObserver {
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    if (state == AppLifecycleState.resumed) {
+      _touchLastActive();
+    }
+  }
+
+  Future<void> _touchLastActive() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setInt(_kLastActiveKey, DateTime.now().millisecondsSinceEpoch);
+  }
+}
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -15,6 +31,7 @@ void main() async {
   runApp(
     UncontrolledProviderScope(container: container, child: const LaundryApp()),
   );
+  WidgetsBinding.instance.addObserver(_AppLifecycleObserver());
   WidgetsBinding.instance.addPostFrameCallback(
     (_) => UpdateChecker.checkForUpdate(),
   );
