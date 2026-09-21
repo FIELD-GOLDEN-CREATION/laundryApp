9c915bcaf5645345d0e2d42055bff14aba8c3011
maxi1000-aura <maximillianjohn315@gmail.com>
2026-09-21 11:48:59 +0300

    refactored review entity

--- lib/models/review_item.dart
+++ lib/models/review_item.dart
@@ -1,6 +1,7 @@
 class ReviewItem {
-  const ReviewItem({required this.name, required this.stars, required this.text});
+  const ReviewItem({required this.id, required this.name, required this.stars, required this.text});
 
+  final String id;
   final String name;
   final String stars;
   final String text;
